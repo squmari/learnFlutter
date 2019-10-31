0fa1f6e7d5896f28050b41033433d1b603b660c4
squmari <squmarigames@gmail.com>
2019-10-31 20:44:46 +0900

    GridList と TabBar実装(+1 squashed commits)
    Squashed commits:
    
    [6e2e692] abBar実装 (+1 squashed commits)
    
    Squashed commits:
    
    [46afa2b] TabBar実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:learn_flutter/widget/gridList.dart';
+import 'package:learn_flutter/widget/tabBar.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  final title = 'Flutter サンプル';
 
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       title: 'Flutter Demo',
-      home: new MyHomePage(
-        title: this.title,
-      ),
-
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  // final String message;
-  final String title;
-
-  MyHomePage({this.title}):super();
-
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _message;
-
-  @override
-  void initState() {
-    super.initState();
-    _message = 'Hello';
-  }
-  
-  void _setMessage(){
-    if(_message == "タップしました"){
-      setState(() {
-        _message = 'Hello';
-      });
-    }
-    else{
-      setState(() {
-        _message = 'タップしました';
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-
-      body: Text(
-        _message,
-        style: TextStyle(fontSize: 32),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _setMessage,
-        tooltip: 'set message',
-        child: Icon(Icons.star),
-      ),
+      home: new MyGridList(100,2),
     );
   }
 }
